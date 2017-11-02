07658047c0aa59a470bcfdbffb5eb0c9568185cd
ProjetoIntegradorIV4 <ifespiv4@outlook.com>
2017-11-02 12:13:20 -0200

    novo

--- PYTHON/cad_ass-server.py
+++ PYTHON/cad_ass-server.py
@@ -1,3 +1,10 @@
+from flask import Flask, render_template
+ 
+app = Flask(__name__)  
+
 @app.route("/teste")
 def index():
 	return render_template('cadastro_associado.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
